0e9af85ac919c4faf09df2fd29d393ac971828a1
emilyjf <flanagan.emily.j@gmail.com>
2019-04-25 15:05:28 -0500

    Create styles.js

---
+++ src/components/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    width: 300,
+    height: 100,
+    marginTop: 20,
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 20
+  }
+});
